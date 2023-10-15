d777e97bbf77b518223694319b20601e177a3d66
ederoliv <ederoliveira152@gmail.com>
2023-10-15 04:19:31 -0300

    Exercise number 001/100

---
+++ src/Exercise001.java
@@ -0,0 +1,12 @@
+/*
+* Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"
+* */
+
+public class Exercise001 {
+
+    public static void main (String[] args) {
+
+        System.out.println("Olá, Mundo!");
+
+    }
+}
