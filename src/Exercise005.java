b8dbaf3aca00f36fb17b6d132f0ab0d322816793
ederoliv <ederoliveira152@gmail.com>
2023-10-18 00:48:10 -0300

    Exercise number 005/100

--- src/Exercise005.java
+++ src/Exercise005.java
@@ -1,5 +1,4 @@
 import java.util.Objects;
-
 /*
     5) Faça um programa que leia as duas notas de um aluno em uma matéria e mostre
 na tela a sua média na disciplina.
@@ -7,7 +6,7 @@ Ex:
 Nota 1: 4.5
 Nota 2: 8.5
 A média entre 4.5 e 8.5 é igual a 6.5
-* */
+*/
 public class Exercise005 {
     public static void main(String[] args) {
 
